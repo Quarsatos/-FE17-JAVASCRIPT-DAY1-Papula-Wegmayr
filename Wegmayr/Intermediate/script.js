9ee7337cfa035ffd923b60c2dc832402d68bdb9a
juliawegmayr <kontakt@juliawegmayr.at>
2022-09-21 11:22:57 +0200

    intermediate exercise

---
+++ Wegmayr/Intermediate/script.js
@@ -0,0 +1,29 @@
+var cars = ['Tesla', 'Audi', 'Renault', 'Volvo', 'Mazda', 'Fiat', 'Ferrari'];
+
+console.log(cars.sort());
+
+
+var fruits= ['apple', 'banana', 'kiwi'];
+
+fruits.push("orange");
+console.log(fruits);
+fruits.pop("orange");
+console.log(fruits);
+
+// Second Exercise
+
+var animals = ['monkey', 'horse', 'dog'];
+
+animals.sort();
+console.log(animals);
+
+animals.splice(0, 0, "cat");
+console.log(animals);
+
+
+// Third Exercise 
+
+var fruit1 = "mango/cherries/kiwi/grapes/pear/peach/orange/lemon";
+
+var fruit2 = fruit1.split("/");
+console.log(fruit2.join('\n'));
