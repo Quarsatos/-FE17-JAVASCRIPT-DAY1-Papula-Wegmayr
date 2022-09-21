06a1e92438e68ccb1d426145948f38371ea201d2
Quarsatos <manuelpapula69@gmail.com>
2022-09-21 11:07:23 +0200

    basic exercises complete

---
+++ Papula/basic-exercise/javascript/main.js
@@ -0,0 +1,6 @@
+// Basic Exercise 2
+document.write("Hi my name is Martin");
+
+// Basic Exercise 4
+const players = ["Martin", "Thomas", "Peter", "Mathias", "Niki"]
+console.log("The most valuable player of the match is " + players[2] + "!") 
