506e8e94703a35eb463c28882349acece9335d8e
juliawegmayr <kontakt@juliawegmayr.at>
2022-09-21 10:48:31 +0200

    Basic Exercise

---
+++ Wegmayr/Basic/script.js
@@ -0,0 +1,7 @@
+console.log("Hi my name is Julia");
+
+var user_age = 25;
+    console.log("Hello from the console and I am " + user_age + " years old.");
+
+    var players = ['Martin', 'Thomas', 'Peter', 'Mathias', 'Niki']
+    console.log(players[2]);
